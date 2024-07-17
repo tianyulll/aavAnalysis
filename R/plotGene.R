5827be6207899755e5f7d8e16d5fd82ba2a67a26
tl <tianyu.lu@pennmedicine.upenn.edu>
2024-07-17 16:22:47 +0000

    add: random plot st.d.

--- R/plotGene.R
+++ R/plotGene.R
@@ -1,13 +1,15 @@
 #' Plot Random Site in Gene and in TU
 #'
 #' @param df df after \code{\link{getGeneDf}}
-#' @param random_exon random sites in Exon percentange
-#' @param random_tu random sites in Transcription Unit Percentage
+#' @param exon_mean mean % of random site in exons
+#' @param tu_mean mean % of random site in transcription unit
+#' @param exon_sd std % of random site in exons
+#' @param tu_sd std % of random site in transcription unit
 #'
 #' @return ggplot2 object
 #' @export plotRandomGene
 #'
-plotRandomGene <- function(df, random_exon = 0, random_tu = 0) {
+plotRandomGene <- function(df, exon_mean = 0, exon_sd = 0, tu_mean = 0, tu_sd = 0) {
 
   df <- df %>%
     mutate(description = paste0(sample,"-", info))
@@ -19,8 +21,10 @@ plotRandomGene <- function(df, random_exon = 0, random_tu = 0) {
     labs(title = "", x = "") +
     scale_x_continuous(labels = scales::label_percent(accuracy = .1, scale = 1),
                        limits = c(0, 100)) +
-    geom_vline(linetype = "dashed", aes(xintercept = random_exon, color = "random exon")) +
-    geom_vline(linetype = "dotted", aes(xintercept = random_tu, color = "random TU")) +
+    geom_vline(linetype = "dashed", aes(xintercept = exon_mean, color = "random exon")) +
+    geom_rect(aes(xmin = exon_mean - exon_sd, xmax = exon_mean + exon_sd, ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.01) +
+    geom_vline(linetype = "dashed", aes(xintercept = tu_mean, color = "random TU")) +
+    geom_rect(aes(xmin = tu_mean - tu_sd, xmax = tu_mean + tu_sd, ymin = -Inf, ymax = Inf), fill = "blue", alpha = 0.01) +
     theme(axis.text = element_text(size = 10),
           axis.title.y = element_blank(),
           panel.grid.major = element_blank(),
